6d6a7c804d2565bf1d736a3a9541f29bac2aa808
uwuohayo <im.cheater.xxx@gmail.com>
2022-10-29 00:38:15 +0400

    init

---
+++ EasyMVP/ConfigManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace EasyMVP
+{
+    public class ConfigManager
+    {
+        public static bool EasyMVP_enable;
+        public static String EasyMVP_messageUp;
+        public static String EasyMVP_messageDown;
+        public static ushort EasyMVP_messageTime;
+
+        public static void registerCfg()
+        {
+            EasyMVP_enable = Plug.Config.GetBool("EasyMVP_enable", true, "on or off EasyMVP?");
+            EasyMVP_messageUp = Plug.Config.GetString("EasyMVP_messageUp", "<color=#FF60A9>MVP</color> on this round - <color=#FF60A9>%mvpNick%</color>", "first bc line");
+            EasyMVP_messageDown = Plug.Config.GetString("EasyMVP_messageDown", "This player has <color=#FF60A9>%mvpKills%</color> kills", "second bc line");
+            EasyMVP_messageTime = Plug.Config.GetUShort("EasyMVP_messageTime", 15, "time to display message");
+        }
+    }
+}
+
---
+++ EasyMVP/Plug.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+using Discord;
+using MEC;
+using Qurre;
+using Qurre.API;
+using Qurre.API.Events;
+using Qurre.Events.Modules;
+using Player = Qurre.API.Player;
+using Round = Qurre.Events.Round;
+using Server = Qurre.Events.Server;
+
+namespace EasyMVP
+{
+    public class Plug : Plugin
+    {
+        public override string Developer => "ohayo!#5601";
+        public override string Name => "EasyMVP";
+        public override Version Version => new Version(1, 0, 0);
+        public override int Priority => int.MinValue;
+
+        public Player mvpPlayer;
+        public int mvpKills;
+
+        private Plug instance;
+        public Plug getInstanse()
+        {
+            return instance;
+        }
+
+        public override void Enable()
+        {
+            instance = this;
+
+            ConfigManager.registerCfg();
+
+            if (ConfigManager.EasyMVP_enable == false)
+            {
+                Log.Error(" > the " + Name + " is disabled because you disabled it in the config");
+                return;
+            }
+
+            Round.Waiting += onWaiting;
+            Round.End += onRoundEnd;
+
+            Log.Info(" " + Name + " enabled :)");
+            Log.Info(" version: " + Version);
+            Log.Info(" dev: " + Developer);
+            Log.Info(" site: www.rootkovskiy.ovh");
+        }
+
+        public override void Disable()
+        {
+            instance = null;
+
+            Round.Waiting -= onWaiting;
+            Round.End -= onRoundEnd;
+
+            Log.Info(" " + Name + " disabled :(");
+            Log.Info(" version: " + Version);
+            Log.Info(" dev: " + Developer);
+            Log.Info(" site: www.rootkovskiy.ovh");
+        }
+
+        public void onWaiting()
+        {
+            mvpPlayer = null;
+            mvpKills = 0;
+        }
+
+        public void onRoundEnd(RoundEndEvent ev)
+        {
+            List<int> playersKillCounts = (from value in Player.List select value.KillsCount).ToList<int>();
+            mvpKills = playersKillCounts.Max();
+            foreach (Player p in Player.List)
+            {
+                if (p.KillsCount == mvpKills)
+                {
+                    mvpPlayer = p;
+                }
+            }
+            foreach (Player p in Player.List)
+            {
+                p.Broadcast(ConfigManager.EasyMVP_messageUp.Replace("%mvpNick%", mvpPlayer.Nickname) + "\n" + ConfigManager.EasyMVP_messageDown.Replace("%mvpKills%", mvpKills.ToString()), ConfigManager.EasyMVP_messageTime);
+            }
+        }
+    }
+}
+
